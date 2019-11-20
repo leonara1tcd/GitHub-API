393aa8b03493cd6e07b18ffdadd4b6fdb18e46db
Alex Leonard <47919688+leonara1tcd@users.noreply.github.com>
2019-11-20 16:40:35 +0000

    Extracting my own info from GitHub API

--- GitHubAPI.R
+++ GitHubAPI.R
@@ -2,6 +2,9 @@
 library(jsonlite)
 library(httpuv)
 library(httr)
+install.packages("plotly")
+require(devtools)
+library(plotly)
 
 oauth_endpoints("github")
 
@@ -28,3 +31,26 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+#Interogating the GitHub API to extract data about my own account and summarise graphically
+
+#Gets my data 
+myData = fromJSON("https://api.github.com/users/leonara1tcd")
+
+#Displays no. of followers
+myData$followers
+
+followers = fromJSON("https://api.github.com/users/leonara1tcd/followers")
+followers$login #Gives user names of all my followers
+
+myData$following #Displays the no. of people I am following
+
+following = fromJSON("https://api.github.com/users/leonara1tcd/following")
+following$login #Gives the names of all the people I am following
+
+myData$public_repos #Displays the no. of repositories I have
+
+repos = fromJSON("https://api.github.com/users/leonara1tcd/repos")
+repos$name #Names of my public repositories
+repos$created_at #Gives details of the dates the repositories were created 
+repos$full_name #gives names of repositories
