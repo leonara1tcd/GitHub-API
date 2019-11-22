3c0da28a448d57543e3d88dbb72079c683036254
Alex Leonard <47919688+leonara1tcd@users.noreply.github.com>
2019-11-22 14:08:32 +0000

    removed my data

--- GitHubAPI.R
+++ GitHubAPI.R
@@ -30,26 +30,6 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 
 #Interogating the GitHub API to extract data about my own account and summarise graphically
 
-#Gets my data 
-myData = fromJSON("https://api.github.com/users/leonara1tcd")
-
-#Displays no. of followers
-myData$followers
-
-followers = fromJSON("https://api.github.com/users/leonara1tcd/followers")
-followers$login #Gives user names of all my followers
-
-myData$following #Displays the no. of people I am following
-
-following = fromJSON("https://api.github.com/users/leonara1tcd/following")
-following$login #Gives the names of all the people I am following
-
-myData$public_repos #Displays the no. of repositories I have
-
-repos = fromJSON("https://api.github.com/users/leonara1tcd/repos")
-repos$name #Names of my public repositories
-repos$created_at #Gives details of the dates the repositories were created 
-repos$full_name #gives names of repositories
 
 myData = GET("https://api.github.com/users/mbostock/followers?per_page=100;", gtoken)
 extract = content(myData)
