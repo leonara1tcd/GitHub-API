564bb71b577be60ac58a4edfb4053eaaa42fe5cb
Alex Leonard <47919688+leonara1tcd@users.noreply.github.com>
2019-11-25 19:41:50 +0000

    Plot 3 Added, Code Reformatted

--- GitHubAPI.R
+++ GitHubAPI.R
@@ -28,12 +28,13 @@ json1 = content(req)
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 
 #Interogating the GitHub API to extract data about my own account and summarise graphically
 
 
-myData = GET("https://api.github.com/users/mbostock/followers?per_page=100;", gtoken)
+myData = GET("https://api.github.com/users/mbostock/followers?per_page=100;",
+             gtoken)
 extract = content(myData)
 stop_for_status(myData)
 githubDB = jsonlite::fromJSON(jsonlite::toJSON(extract))
@@ -54,16 +55,20 @@ id = githubDB$login
 user_ids = c(id)
 
 #Loops through users and adds to list
-for(i in 1:length(user_ids))
+for (i in 1:length(user_ids))
 {
-  followingURL = paste("https://api.github.com/users/", user_ids[i], "/following", sep = "")
+  followingURL = paste("https://api.github.com/users/",
+                       user_ids[i],
+                       "/following",
+                       sep = "")
   followingRequest = GET(followingURL, gtoken)
   followingContent = content(followingRequest)
   
-  if(length(followingContent) == 0)
+  if (length(followingContent) == 0)
   {
     next
-  } 
+    
+  }
   followingDF = jsonlite::fromJSON(jsonlite::toJSON(followingContent))
   followingLogin = followingDF$login
   
@@ -85,20 +90,26 @@ for(i in 1:length(user_ids))
       #Retrieving users followers
       followersNumber = followingDF2$followers
       
-      #Retrieving how many repository the user has 
+      #Retrieving how many repository the user has
       reposNumber = followingDF2$public_repos
       
       #Retrieving year which each user joined Github
-      yearCreated = substr(followingDF2$created_at, start = 1, stop = 4)
+      yearCreated = substr(followingDF2$created_at,
+                           start = 1,
+                           stop = 4)
       
       #Add users data to a new row in dataframe
-      usersDB[nrow(usersDB) + 1, ] = c(followingLogin[j], followingNumber, followersNumber, reposNumber, yearCreated)
+      usersDB[nrow(usersDB) + 1,] = c(followingLogin[j],
+                                      followingNumber,
+                                      followersNumber,
+                                      reposNumber,
+                                      yearCreated)
       
     }
     next
   }
   #Stop when there are more than 150 users
-  if(length(users) > 150)
+  if (length(users) > 150)
   {
     break
   }
@@ -106,25 +117,95 @@ for(i in 1:length(user_ids))
 }
 
 #Created link to plotly which creates online interactive graphs.
-Sys.setenv("plotly_username"="leonara1")
-Sys.setenv("plotly_api_key"="yiFJeOMQXwYvetnbAdYq")
+Sys.setenv("plotly_username" = "leonara1")
+Sys.setenv("plotly_api_key" = "yiFJeOMQXwYvetnbAdYq")
 
 #Plot 1 graphs repositories vs followers by year.
 #The data is represented by a scatter plot.
 #X-axis displays 'repositories' which shows the no. of repositories per user.
 #Y-axis displays 'followers' which shows the no. of followers of each each of mbostock's followers.
-plot1 = plot_ly(data = usersDB, x = ~repos, y = ~followers, text = ~paste("Followers: ", followers, "<br>Repositories: ", repos, "<br>Date Created:", dateCreated), color = ~dateCreated)
+plot1 = plot_ly(
+  data = usersDB,
+  x = ~ repos,
+  y = ~ followers,
+  text = ~ paste(
+    "Followers: ",
+    followers,
+    "<br>Repositories: ",
+    repos,
+    "<br>Date Created:",
+    dateCreated
+  ),
+  color = ~ dateCreated
+)
 plot1
 
 #Sends graph to plotly - located at https://plot.ly/~leonara1/1/#/
-api_create(plot1, filename= "Followers vs Repos")
+api_create(plot1, filename = "Followers vs Repos")
 
 #Plot 2 graphs following vs followers by year.
 #The data is represented by a scatter plot.
 #X-axis displays 'following' which shows the no. of users followed by each of mbostock's followers.
 #Y-axis displays 'followers' which shows the no. of followers of each of mbostock's followers.
-plot2 = plot_ly(data = usersDB, x = ~following, y = ~followers, text = ~paste("Followers: ", followers, "<br>Following: ", following), color = ~dateCreated)
+plot2 = plot_ly(
+  data = usersDB,
+  x = ~ following,
+  y = ~ followers,
+  text = ~ paste("Followers: ", followers, "<br>Following: ", following),
+  color = ~ dateCreated
+)
 plot2
 
 #Sends graph to plotly - located at https://plot.ly/~leonara1/3/#/
 api_create(plot2, filename = "Following vs Followers")
+
+#Plot 3 shows the 10 most popular languages used by mbostock's followers
+#The data is represented by a bar chart.
+#X-axis displays languages.
+#Y-axis displays number of users.
+languages = c()
+
+for (i in 1:length(users))
+{
+  RepositoriesUrl = paste("https://api.github.com/users/", users[i], "/repos", sep = "")
+  Repositories = GET(RepositoriesUrl, gtoken)
+  RepositoriesContent = content(Repositories)
+  RepositoriesDF = jsonlite::fromJSON(jsonlite::toJSON(RepositoriesContent))
+  RepositoriesNames = RepositoriesDF$name
+  
+  for (j in 1:length(RepositoriesNames))
+  {
+    RepositoriesUrl2 = paste("https://api.github.com/repos/",
+                             users[i],
+                             "/",
+                             RepositoriesNames[j],
+                             sep = "")
+    Repositories2 = GET(RepositoriesUrl2, gtoken)
+    RepositoriesContent2 = content(Repositories2)
+    RepositoriesDF2 = jsonlite::fromJSON(jsonlite::toJSON(RepositoriesContent2))
+    language = RepositoriesDF2$language
+    
+    if (length(language) != 0 && language != "<NA>")
+    {
+      languages[length(languages) + 1] = language
+    }
+    next
+  }
+  next
+}
+
+allLanguages = sort(table(languages), increasing = TRUE)
+top10Languages = allLanguages[(length(allLanguages) - 9):length(allLanguages)]
+
+languageDF = as.data.frame(top10Languages)
+
+plot3 = plot_ly(
+  data = languageDF,
+  x = languageDF$languages,
+  y = languageDF$Freq,
+  type = "bar"
+)
+plot3
+
+#Sends graph to plotly - located at https://plot.ly/~leonara1/7/#/
+api_create(plot3, filename = "Ten Most Popular Languages")
